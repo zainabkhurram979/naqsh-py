ca675d5dd8126777e44034e2bd6ca8b4302e97ff
Zainab Khurram <zainabkhurram979@gmail.com>
2026-07-27 22:30:00 +0500

    Add star pattern example

--- examples/star_pattern.py
+++ examples/star_pattern.py
@@ -1,1 +1,13 @@
+import nakshpy as nk
 
+# Generate an 8-point star
+star = nk.star_pattern(points=8, outer_radius=2, inner_radius=1)
+
+# Rotate it
+star = nk.rotate(star, 30)
+
+# Draw it
+nk.draw(star)
+
+# Save it
+nk.save("star_pattern.png")
