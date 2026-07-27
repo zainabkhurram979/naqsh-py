ae7e233d489daa6abfc5f45bf6f918900c2ca1f1
Zainab Khurram <zainabkhurram979@gmail.com>
2026-07-27 22:37:56 +0500

    Add geometry tests

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,1 +1,13 @@
+from nakshpy.geometry import Point, Line
 
+def test_distance():
+    p1 = Point(0, 0)
+    p2 = Point(3, 4)
+
+    assert p1.distance_to(p2) == 5
+
+
+def test_line_length():
+    line = Line(Point(0, 0), Point(3, 4))
+
+    assert line.length() == 5
