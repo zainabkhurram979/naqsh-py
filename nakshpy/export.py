eadf8616ff30e68c0c413aa7643f4c618c1aa595
Zainab Khurram <zainabkhurram979@gmail.com>
2026-07-27 22:29:03 +0500

    Add export module

--- nakshpy/export.py
+++ nakshpy/export.py
@@ -1,1 +1,13 @@
+"""
+export.py
+Save NakshPy patterns as images.
+"""
 
+import matplotlib.pyplot as plt
+
+
+def save(filename="pattern.png"):
+    """
+    Save the current figure.
+    """
+    plt.savefig(filename, dpi=300, bbox_inches="tight")
